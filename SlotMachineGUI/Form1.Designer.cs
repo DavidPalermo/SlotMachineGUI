dd14ced982996f240d9ca7b8de67df616ed6f5b1
DavidPalermo <palermo.dj@hotmail.com>
2016-04-24 11:35:09 -0500

    Add project files.

---
+++ SlotMachineGUI/Form1.Designer.cs
@@ -0,0 +1,169 @@
+﻿namespace SlotMachineGUI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.imgBox1 = new System.Windows.Forms.PictureBox();
+            this.imgBox2 = new System.Windows.Forms.PictureBox();
+            this.imgBox3 = new System.Windows.Forms.PictureBox();
+            this.btnPlay = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.lblPoints = new System.Windows.Forms.Label();
+            this.lblResults = new System.Windows.Forms.Label();
+            this.lblJackpot = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.imgBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imgBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imgBox3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // imgBox1
+            // 
+            this.imgBox1.Image = global::SlotMachineGUI.Properties.Resources.sealsinger;
+            this.imgBox1.Location = new System.Drawing.Point(12, 66);
+            this.imgBox1.Name = "imgBox1";
+            this.imgBox1.Size = new System.Drawing.Size(252, 273);
+            this.imgBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.imgBox1.TabIndex = 0;
+            this.imgBox1.TabStop = false;
+            // 
+            // imgBox2
+            // 
+            this.imgBox2.Image = ((System.Drawing.Image)(resources.GetObject("imgBox2.Image")));
+            this.imgBox2.Location = new System.Drawing.Point(285, 66);
+            this.imgBox2.Name = "imgBox2";
+            this.imgBox2.Size = new System.Drawing.Size(246, 273);
+            this.imgBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.imgBox2.TabIndex = 1;
+            this.imgBox2.TabStop = false;
+            // 
+            // imgBox3
+            // 
+            this.imgBox3.Image = ((System.Drawing.Image)(resources.GetObject("imgBox3.Image")));
+            this.imgBox3.Location = new System.Drawing.Point(557, 66);
+            this.imgBox3.Name = "imgBox3";
+            this.imgBox3.Size = new System.Drawing.Size(246, 273);
+            this.imgBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.imgBox3.TabIndex = 2;
+            this.imgBox3.TabStop = false;
+            // 
+            // btnPlay
+            // 
+            this.btnPlay.AutoSize = true;
+            this.btnPlay.Font = new System.Drawing.Font("Segoe Script", 12F, ((System.Drawing.FontStyle)(((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic) 
+                | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPlay.Location = new System.Drawing.Point(361, 397);
+            this.btnPlay.Name = "btnPlay";
+            this.btnPlay.Size = new System.Drawing.Size(75, 43);
+            this.btnPlay.TabIndex = 3;
+            this.btnPlay.Text = "Play";
+            this.btnPlay.UseVisualStyleBackColor = true;
+            this.btnPlay.Click += new System.EventHandler(this.btnPlay_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.AutoSize = true;
+            this.btnClear.Font = new System.Drawing.Font("Segoe Script", 12F, ((System.Drawing.FontStyle)(((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic) 
+                | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClear.Location = new System.Drawing.Point(605, 522);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(198, 43);
+            this.btnClear.TabIndex = 4;
+            this.btnClear.Text = "Clear my score...";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Visible = false;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // lblPoints
+            // 
+            this.lblPoints.AutoSize = true;
+            this.lblPoints.Font = new System.Drawing.Font("Segoe Script", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPoints.Location = new System.Drawing.Point(12, 533);
+            this.lblPoints.Name = "lblPoints";
+            this.lblPoints.Size = new System.Drawing.Size(0, 32);
+            this.lblPoints.TabIndex = 5;
+            // 
+            // lblResults
+            // 
+            this.lblResults.AutoSize = true;
+            this.lblResults.Font = new System.Drawing.Font("Segoe Script", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblResults.Location = new System.Drawing.Point(15, 464);
+            this.lblResults.Name = "lblResults";
+            this.lblResults.Size = new System.Drawing.Size(0, 32);
+            this.lblResults.TabIndex = 6;
+            // 
+            // lblJackpot
+            // 
+            this.lblJackpot.AutoSize = true;
+            this.lblJackpot.BackColor = System.Drawing.Color.Transparent;
+            this.lblJackpot.Font = new System.Drawing.Font("Monotype Corsiva", 13.8F, ((System.Drawing.FontStyle)(((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic) 
+                | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblJackpot.ForeColor = System.Drawing.Color.Black;
+            this.lblJackpot.Location = new System.Drawing.Point(310, 9);
+            this.lblJackpot.Name = "lblJackpot";
+            this.lblJackpot.Size = new System.Drawing.Size(0, 28);
+            this.lblJackpot.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::SlotMachineGUI.Properties.Resources.SealHabitat;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(815, 577);
+            this.Controls.Add(this.lblJackpot);
+            this.Controls.Add(this.lblResults);
+            this.Controls.Add(this.lblPoints);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnPlay);
+            this.Controls.Add(this.imgBox3);
+            this.Controls.Add(this.imgBox2);
+            this.Controls.Add(this.imgBox1);
+            this.Name = "Form1";
+            this.Text = "Slot Machine GUI - David Palermo";
+            ((System.ComponentModel.ISupportInitialize)(this.imgBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imgBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imgBox3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox imgBox1;
+        private System.Windows.Forms.PictureBox imgBox2;
+        private System.Windows.Forms.PictureBox imgBox3;
+        private System.Windows.Forms.Button btnPlay;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Label lblPoints;
+        private System.Windows.Forms.Label lblResults;
+        private System.Windows.Forms.Label lblJackpot;
+    }
+}
+
---
+++ SlotMachineGUI/Form1.cs
@@ -0,0 +1,110 @@
+﻿using SlotMachineGUI.Properties;
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace SlotMachineGUI
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            lblPoints.Text = "Points: 200";
+        }
+        int img1, img2, img3, winVal, points = 200, jackpot = 200;        
+        string calcOutVal;
+        const int playCost = -10;        
+        Random rnd = new Random();
+        Image[] imgBoxPics = {Resources.AwkwardSeal, Resources.seal1, Resources.seal2, Resources.seal3, Resources.seal4,
+                                 Resources.seal5, Resources.seal6, Resources.seal7, Resources.seal8, Resources.sealsinger};
+       
+        /*
+        # btnPlay_Click is the Main driving method of this app,
+        # it increases and updates the Jackpot per click.
+        */
+        private void btnPlay_Click(object sender, EventArgs e)
+        {
+            jackpot += 10;
+            lblJackpot.Text = String.Format("Jackpot is {0} points", jackpot);
+            lblPoints.Text = calcPoints(playCost);
+            lblResults.Text = "";
+            PlayCost();            
+            if (points < 10)
+            {
+                btnPlay.Visible = false;
+                btnClear.Visible = true;
+                lblResults.Text = "You Lose!";
+            }
+        }
+
+        /*
+        # PlayCost method checks for any matching pictures that are awarded points, 
+        # establishes the amount of points to be awarded, passes the award value 
+        # to the calcPoints method, and lastly informs the User of what happened
+        # through the lblPoints and lblResults labels as necessary.
+        */
+        private void PlayCost()
+        {
+            imgBox1.Image = imgBoxPics[rndelement(ref img1)];
+            imgBox2.Image = imgBoxPics[rndelement(ref img2)];
+            imgBox3.Image = imgBoxPics[rndelement(ref img3)];
+            if (img1 == 0 && img2 == 0 && img3 == 0)
+            {
+                lblResults.Text = String.Format("You hit the jackpot! \n+ {0}", jackpot);
+                lblPoints.Text = calcPoints(jackpot);
+                jackpot = 200;
+            }
+            else if (img1 == img2 && img1 == img3)
+            {
+                winVal = rnd.Next(55, 75);
+                lblResults.Text = String.Format("You won! \n+ {0}", winVal);
+                lblPoints.Text = calcPoints(winVal);
+            }
+            else if (img1 == img2 || img1 == img3 || img2 == img3)
+            {
+                winVal = rnd.Next(20, 30);
+                lblResults.Text = String.Format("You won! \n+ {0}", winVal);
+                lblPoints.Text = calcPoints(winVal);
+            }
+            else
+                lblResults.Text = "Sorry, you did not win.";
+        }        
+
+        /*
+        # Method to adjust the User's points
+        # @param int numIn passed in from the PlayCost method to be converted to a string
+        # @return string calcOutVal as a formated string representation of points.
+        */
+        private string calcPoints(int numIn)
+        {
+            points += numIn;
+            calcOutVal = String.Format("Points: {0}", points); 
+            return calcOutVal;
+        }
+
+        /*
+        # Method to randomly pick a number based off the imgBoxPics array
+        # @param ref int num passed in to be assigned the random pick 
+        # @return num is the randomly picked number.
+        */
+        private int rndelement(ref int num)
+        {
+            num = rnd.Next(0, imgBoxPics.Length);
+            return num;
+        }
+
+        /*
+        # Method to reset the application for further play. 
+        # Resets the User's points and posts it to the lblPoints label.         
+        */
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            points = 200;
+            btnClear.Visible = false;
+            btnPlay.Visible = true;
+            lblResults.Text = "";
+            lblPoints.Text = String.Format("Points: {0}", points);
+        }
+    }
+}
